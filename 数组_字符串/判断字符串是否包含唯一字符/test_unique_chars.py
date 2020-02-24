5703defc182388d18513c34d6ffd5199a06a0191
skrBang <s_huabang@163.com>
2020-02-24 15:59:05 +0800

    判断字符串中的字符是否唯一

--- 数组_字符串/判断字符串是否包含唯一字符/test_unique_chars.py
+++ 数组_字符串/判断字符串是否包含唯一字符/test_unique_chars.py
@@ -1,30 +1,26 @@
 from nose.tools import assert_equal
 
-
-class TestUniqueChars(object):
-
+class TestUniqueChars:
+    
     def test_unique_chars(self, func):
         assert_equal(func(None), False)
         assert_equal(func(''), True)
         assert_equal(func('foo'), False)
         assert_equal(func('bar'), True)
-        print('Success: test_unique_chars')
-
-
+        print('Success: test_unique_chars!')
+        
 def main():
     test = TestUniqueChars()
     unique_chars = UniqueChars()
     test.test_unique_chars(unique_chars.has_unique_chars)
     try:
         unique_chars_set = UniqueCharsSet()
+        unique_chars_in_place = UniquecharsInPlace()
         test.test_unique_chars(unique_chars_set.has_unique_chars)
-        unique_chars_in_place = UniqueCharsInPlace()
         test.test_unique_chars(unique_chars_in_place.has_unique_chars)
     except NameError:
-        # Alternate solutions are only defined
-        # in the solutions file
         pass
-
-
+    
 if __name__ == '__main__':
     main()
+    
